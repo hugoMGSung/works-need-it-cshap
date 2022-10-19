4ae37b338336e54cb0525bd6f239dbbf130f3c46
Hugo MG Sung <personar95@naver.com>
2022-10-19 15:45:39 +0900

    새로 하는 강의 올리기

---
+++ basic-csharp/HelloWorldApp/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldApp
+{
+    internal class HelloWorld
+    {
+        // 엔트리 포인트
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteLine("사용법 : HelloWorldApp.exe <이름>");
+                return;
+            }
+
+            WriteLine($"Hello, {args[0]}");
+        }
+    }
+}
